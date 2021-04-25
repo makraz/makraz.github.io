219469ac29887f5ff0da659c2178666d64c66640
makraz <makraz@users.noreply.github.com>
2021-04-25 16:40:16 +0000

    deploy: 9a9b2a39118f87dd7bf4c127bf1e2fe21510b4b8

--- _nuxt/static/1619368464/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619368464",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bb4f402e"}}}}(null,"\u002F"));
