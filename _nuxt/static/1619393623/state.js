38db39e8394e070bb9f9dadfadd6e385f1bedce3
makraz <makraz@users.noreply.github.com>
2021-04-26 00:40:20 +0000

    deploy: c39a9a06b910fdacd2b26cb876e21fa4787ce246

--- _nuxt/static/1619393623/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619393623",layout:"default",error:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7537da03"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1619397619/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619397619",layout:"default",error:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"333f4c44"}}}}(null,"\u002F"));
