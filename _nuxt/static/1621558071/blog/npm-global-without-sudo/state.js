996ce252619b695ce75651622b95d0dc739a05d2
makraz <makraz@users.noreply.github.com>
2021-05-21 00:47:53 +0000

    deploy: d73a38a41f6ed2d48cc57b85f32038501dffef25

---
+++ _nuxt/static/1621558071/blog/npm-global-without-sudo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621558071",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fnpm-global-without-sudo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ee8e96b8"}}}}(null));
