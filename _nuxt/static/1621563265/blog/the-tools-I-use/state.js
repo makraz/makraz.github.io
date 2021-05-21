74ed5195a5c2a7508ffe5f0b626a77b907a47dda
makraz <makraz@users.noreply.github.com>
2021-05-21 02:14:27 +0000

    deploy: b76bf4d507280e7348e59ab75535fad13f2fce11

---
+++ _nuxt/static/1621563265/blog/the-tools-I-use/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621563265",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fthe-tools-I-use",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"09b4af3c"}},_img:{"/_ipx/makraz.png?f=webp&s=128_47":"\u002F_nuxt\u002Fimage\u002F87f0ca.webp"}}}(null));
