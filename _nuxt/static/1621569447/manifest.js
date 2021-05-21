71de1f33029a92eee3ff55e912a2dfe368f78617
makraz <makraz@users.noreply.github.com>
2021-05-21 03:57:30 +0000

    deploy: fcec429914465aa01379bc5729701d47f1569116

---
+++ _nuxt/static/1621569447/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fblog","\u002Fget-in-touch","\u002Fmaintenance","\u002Ftags","\u002Ftalks","\u002Fblog\u002Fcoming-soon","\u002Fblog\u002Fthe-tools-I-use","\u002Fblog\u002Fnpm-global-without-sudo"]})
